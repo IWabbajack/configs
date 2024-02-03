1838ebd7d0163e66c07c39a274e2eb8846a71a70
Eduard Ispas <edis@Eduards-MacBook-Air.local>
2024-02-03 11:35:33 +0100

    Pyright doesn't work properly on mac

--- nvim/init.lua
+++ nvim/init.lua
@@ -310,64 +310,64 @@ end, 0)
 require('neodev').setup()
 
 local lspconfig = require('lspconfig')
-lspconfig.pyright.setup {
-  {
-    python = {
-      analysis = {
-        autoSearchPaths = "true",
-        diagnosticMode = "openFilesOnly",
-        useLibraryCodeForTypes = true,
-        typeCheckingMode = "strict",
-      }
-    }
-  }
-}
--- lspconfig.jedi_language_server.setup({
---   init_options = {
---     codeAction = {
---           nameExtractVariable = "jls_extract_var",
---           nameExtractFunction = "jls_extract_def",
---         },
---         completion = {
---           disableSnippets = false,
---           resolveEagerly = false,
---           ignorePatterns = {},
---         },
---         diagnostics = {
---           enable = true,
---           didOpen = true,
---           didChange = true,
---           didSave = true,
---         },
---         hover = {
---           enable = true,
---           disable = {
---             class = { all = false, names = {}, fullNames = {} },
---             ["function"] = { all = false, names = {}, fullNames = {} },
---             instance = { all = false, names = {}, fullNames = {} },
---             keyword = { all = false, names = {}, fullNames = {} },
---             module = { all = false, names = {}, fullNames = {} },
---             param = { all = false, names = {}, fullNames = {} },
---             path = { all = false, names = {}, fullNames = {} },
---             property = { all = false, names = {}, fullNames = {} },
---             statement = { all = false, names = {}, fullNames = {} },
---           },
---         },
---         jediSettings = {
---           autoImportModules = {},
---           caseInsensitiveCompletion = true,
---           debug = false,
---         },
---         markupKindPreferred = "markdown",
---         workspace = {
---           extraPaths = { "/home/eduard/devbox/ouroboros/swint" },
---           symbols = {
---             ignoreFolders = { ".nox", ".tox", ".venv", "__pycache__", "venv" },
---             maxSymbols = 20,
---           },
---         },
+-- lspconfig.pyright.setup {
+--   {
+--     python = {
+--       analysis = {
+--         autoSearchPaths = "true",
+--         diagnosticMode = "openFilesOnly",
+--         useLibraryCodeForTypes = true,
+--         typeCheckingMode = "strict",
+--       }
+--     }
 --   }
--- })
+-- }
+lspconfig.jedi_language_server.setup({
+  init_options = {
+    codeAction = {
+          nameExtractVariable = "jls_extract_var",
+          nameExtractFunction = "jls_extract_def",
+        },
+        completion = {
+          disableSnippets = false,
+          resolveEagerly = false,
+          ignorePatterns = {},
+        },
+        diagnostics = {
+          enable = true,
+          didOpen = true,
+          didChange = true,
+          didSave = true,
+        },
+        hover = {
+          enable = true,
+          disable = {
+            class = { all = false, names = {}, fullNames = {} },
+            ["function"] = { all = false, names = {}, fullNames = {} },
+            instance = { all = false, names = {}, fullNames = {} },
+            keyword = { all = false, names = {}, fullNames = {} },
+            module = { all = false, names = {}, fullNames = {} },
+            param = { all = false, names = {}, fullNames = {} },
+            path = { all = false, names = {}, fullNames = {} },
+            property = { all = false, names = {}, fullNames = {} },
+            statement = { all = false, names = {}, fullNames = {} },
+          },
+        },
+        jediSettings = {
+          autoImportModules = {},
+          caseInsensitiveCompletion = true,
+          debug = false,
+        },
+        markupKindPreferred = "markdown",
+        workspace = {
+          extraPaths = { "/home/eduard/devbox/ouroboros/swint" },
+          symbols = {
+            ignoreFolders = { ".nox", ".tox", ".venv", "__pycache__", "venv" },
+            maxSymbols = 20,
+          },
+        },
+  }
+})
 
 lspconfig.lua_ls.setup {}
 lspconfig.als.setup {
